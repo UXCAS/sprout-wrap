0af5a3b3fedc0e5c22d5a97cdbe57b6c129288c0
Jeff Morgan <jeffrey.morgan@samsung.com>
2014-01-08 11:31:50 -0800

    Fixed problems with ADT metadata

---
+++ site-cookbooks/adt/metadata.rb
@@ -0,0 +1,9 @@
+# Encoding: utf-8
+name             'adt'
+maintainer       'Samsung SRA'
+maintainer_email 'uxcasdeveloper@gmail.com'
+license          'Apache 2.0'
+description      'Cookbook to install ADT on MacOSX'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
+supports         'mac_os_x'
