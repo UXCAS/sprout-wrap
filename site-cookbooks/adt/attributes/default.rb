d14fd863f2d16cc5579fcef0bbade50c0b837d70
Jeff Morgan <jeffrey.morgan@samsung.com>
2014-01-08 10:57:43 -0800

    Added an ADT recipe

---
+++ site-cookbooks/adt/attributes/default.rb
@@ -0,0 +1,2 @@
+default['adt']['cache_dir'] = Chef::Config[:file_cache_path]
+default['adt']['source_url'] = "http://dl.google.com/android/adt/adt-bundle-mac-x86_64-20131030.zip"
---
+++ site-cookbooks/adt/recipes/default.rb
@@ -0,0 +1,6 @@
+ark 'adt' do
+  path node["adt"]["dest_dir"]
+  url node["adt"]["source_url"]
+  checksum '6d0c5690f11f92e114d4fd6ec481429e3b53685e07aa2baa820ec8e62786e4c6'
+  action :put
+end
